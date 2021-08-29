0d1463fa2563129d121bccc7638663e111fd9ff9
sossou oronce <freddysoso4@gmail.com>
2021-08-29 09:29:14 +0100

    Test: add readAllUserTest and readByIdTest

--- src/test/java/org/hibernate/UserModel/dbOperationsTest.java
+++ src/test/java/org/hibernate/UserModel/dbOperationsTest.java
@@ -1,6 +1,9 @@
 package org.hibernate.UserModel;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import java.util.List;
 
 import com.userController.User;
 
@@ -13,10 +16,16 @@ public class dbOperationsTest {
     @Test
     public void createUserTest() {
         int id = 0;
-        User user = new User("lol", "haha");
+        User user = new User("lo", "haha");
         id = dbOperations.CreateUser(user);
         assertTrue("id must be great than 0", id > 0);
     }
-
+    
+    @Test
+    public void readAllUser() {
+        List<User> users = null;
+        users = dbOperations.readAllUser();
+        assertFalse(users.isEmpty());
+    }
     
 }
